a01a037ff6929a710a94bbd3345d24415a5ff6a8
= <=>
2024-12-29 17:10:16 +0100

    fix: updated logo

--- pages/login.js
+++ pages/login.js
@@ -1,49 +1,50 @@
 import { Button } from '@material-ui/core';
-import  Head  from 'next/head';
-import styled from 'styled-components'
+import Head from 'next/head';
+import styled from 'styled-components';
 import { auth, provider } from '../firebase';
 
 function login() {
-
-const signIn = () => {
+  const signIn = () => {
     auth.signInWithPopup(provider).catch(alert);
-}
+  };
 
   return (
     <Container>
-        <Head>
-            <title>Login</title>
-        </Head>
+      <Head>
+        <title>Login</title>
+      </Head>
 
-        <LoginContainer>
-            <Logo src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"/>
-            <Button onClick={signIn} variant='outlined'>Sign in with Google</Button>
-        </LoginContainer>
+      <LoginContainer>
+        <Logo src="/img/ChatChat.jpg" />
+        <Button onClick={signIn} variant="outlined">
+          Sign in with Google
+        </Button>
+      </LoginContainer>
     </Container>
-  )
+  );
 }
 
-export default login
+export default login;
 
 const Container = styled.div`
-    display: grid;
-    place-items: center;
-    height: 100vh;
-    background-color: whitesmoke
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: whitesmoke;
 `;
 
 const LoginContainer = styled.div`
-padding: 100px;
-align-items:center;
-background-color: white;
-display: flex;
-flex-direction: column;
-border-radius: 20px;
-box-shadow: 8px 4px 14px -3px rgba(0,0,0,0.7);
+  padding: 100px;
+  align-items: center;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  box-shadow: 8px 4px 14px -3px rgba(0, 0, 0, 0.7);
 `;
 
 const Logo = styled.img`
-    height: 200px;
-    width: 200px;
-    margin-bottom: 50px;
+  height: 200px;
+  width: 200px;
+  margin-bottom: 50px;
 `;
